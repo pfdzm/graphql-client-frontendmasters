f0f00f6df4a0a2627189314d8cd37823d8003cc9
pfdzm <pfdzm@users.noreply.github.com>
2021-01-10 18:49:56 +0100

    Add basic query and mutation

--- client/src/pages/Pets.js
+++ client/src/pages/Pets.js
@@ -1,21 +1,62 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import gql from 'graphql-tag'
 import { useQuery, useMutation } from '@apollo/react-hooks'
 import PetsList from '../components/PetsList'
 import NewPetModal from '../components/NewPetModal'
 import Loader from '../components/Loader'
 
+const ALL_PETS = gql`
+  query AllPets {
+    pets {
+      id
+      name
+      type
+      img
+    }
+  }
+`
 
-export default function Pets () {
+const CREATE_PET = gql`
+  mutation AddPet($newPet: NewPetInput!) {
+    addPet(input: $newPet) {
+      id
+      name
+      type
+      img
+    }
+  }
+`
+
+export default function Pets() {
   const [modal, setModal] = useState(false)
+  const { data, loading, error } = useQuery(ALL_PETS)
+  const [
+    createPet,
+    { createPetData, createPetLoading, createPetError },
+  ] = useMutation(CREATE_PET)
 
-
-  const onSubmit = input => {
+  const onSubmit = (input) => {
+    createPet({
+      variables: {
+        newPet: {
+          name: input.name,
+          type: input.type,
+        },
+      },
+    })
     setModal(false)
   }
-  
+
   if (modal) {
     return <NewPetModal onSubmit={onSubmit} onCancel={() => setModal(false)} />
+  }
+
+  if (loading || createPetLoading) {
+    return <Loader />
+  }
+
+  if (error || createPetLoading) {
+    return <h1>Error!</h1>
   }
 
   return (
@@ -32,7 +73,7 @@ export default function Pets () {
         </div>
       </section>
       <section>
-        <PetsList />
+        <PetsList pets={data.pets} />
       </section>
     </div>
   )
